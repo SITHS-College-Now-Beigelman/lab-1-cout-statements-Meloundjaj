5c8202da0bea83fa38d9a360feed163d1f34254b
Meloundjaj <meloundjaj@nycstudents.net>
2024-09-16 15:12:17 -0400

    hi
    hi

--- Joseph_Meloundja_Lab_1.cpp
+++ Joseph_Meloundja_Lab_1.cpp
@@ -1,35 +1,39 @@
 //Meloundja Joseph 
 //Lab One
 //09/10/2024
+
 #include<iostream>
+
 using namespace std;
 
 int main()
 {
     cout << "#######################################################################################" << endl; 
     cout << "#                                                                                     #" << endl;
-  
-
-    
     cout << "# Ways to access the Task Manager on your Windows computer:                           #" << endl;
-
-    cout << "#                                                                                     #" << endl;
-   
-   
- 
-    
+    cout << "#                                                                                     #" << endl; // your able to have nothing within the quotations 
     cout << "#            Press the key combination Ctrl + Shift + Escape                          #" << endl;
     cout << "#                                                                                     #" << endl;
-  
-    cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl;
+    cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl; // i want sushi?
     cout << "#                                                                                     #" << endl;
-   
     cout << "#            Type “Task Manager” in the Windows Start menu search                     #" << endl;
-    
     cout << "#                                                                                     # " << endl;
-   
-    cout << "#######################################################################################" << endl;
+    cout << "#######################################################################################" << endl; //hi 
+    
+    
+/*#######################################################################################
+  #                                                                                     #
+  # Ways to access the Task Manager on your Windows computer:                           #
+  #                                                                                     #
+  #            Press the key combination Ctrl + Shift + Escape                          #
+  #                                                                                     #
+  #            Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #
+  #                                                                                     #
+  #            Type “Task Manager” in the Windows Start menu search                     #
+  #                                                                                     # 
+  #######################################################################################*/
   
+  return 0;
 
     
 }
